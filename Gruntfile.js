394013938275e2b15a7f0ea89c457fe2912992c5
pawelgur <pawelgur@gmail.com>
2014-11-09 21:01:30 +0200

    Reorganized project structure. Added: grunt, comments.

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat :{
+			mixins : {
+				files : {
+					'dist/_mixins.less' : 'src/*.less'
+				}
+			}
+		},
+		watch : {
+			less : {
+				files : ['src/*.less'],
+				tasks : ['less', 'clean',  'concat']
+			}
+		},
+		less : {	//scans less files for errors
+			mixins : {
+				files : {
+					'src/temp.css' : "src/*.less"
+				}
+			}
+		},
+		clean : ['src/temp.css']
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+
+	grunt.registerTask('default', ['less', 'clean', 'concat']);
+};
--- README.md
+++ README.md
@@ -3,4 +3,10 @@ less-mixins
 
 Various usefull less mixins.
 
-USAGE: include (as reference) "_mixins.less" in your .less files.
+####Usage
+Copy *dist/_mixins.less* to your project and include it (as reference) in your .less files.
+
+####Bower
+Install from command line
+
+`bower install pg-less-mixins --save-dev`
